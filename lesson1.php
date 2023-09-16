590897f0b6d5b68782bb66a2798e5daff66ab571
Aleksandr Kargin <AKargin@korusconsulting.ru>
2023-09-16 15:59:00 +0300

    рендер атрибутов

--- lesson1.php
+++ lesson1.php
@@ -2,14 +2,43 @@
 
 class Tag
 {
+    protected string $name;
+    protected array $arAttributes;
+    protected array $arChild;
+
+    public function attr($key, $value)
+    {
+        $this->arAttributes[$key] = $value;
+    }
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
 }
 
 class SingleTag extends Tag
 {
+    public function render()
+    {
+        return "<" . $this->name . ">";
+    }
 }
 
 class PairTag extends Tag
 {
+    public function appendChild(Tag $item)
+    {
+        $this->arChild[] = $item;
+    }
+
+    public function render()
+    {
+        foreach ($this->arChild as $item) {
+            $content .= $item->render();
+        }
+        return "<" . $this->name . "> " . $content . "  </" . $this->name . ">";
+    }
 }
 
 $img = new SingleTag('img');
